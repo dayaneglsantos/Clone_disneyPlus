7b13b95998c746d088762207653d1bdf31dde4fb
Dayane Santos <dayane0204@hotmail.com>
2023-05-30 14:44:04 -0300

    Conclusão do hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require("gulp-sourcemaps");
+const imagemin = require("gulp-imagemin");
 
 function compSass() {
   return gulp
@@ -11,7 +12,14 @@ function compSass() {
     .pipe(gulp.dest("./build/styles"));
 }
 
-exports.default = compSass;
+function images() {
+  return gulp
+    .src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./build/images"));
+}
+
+exports.default = gulp.parallel(compSass, images);
 exports.watch = function () {
   gulp.watch("./src/styles/*.scss", gulp.parallel(compSass));
 };
